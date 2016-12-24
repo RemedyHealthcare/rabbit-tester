c59dd14755d2b0b73e5955a63527655858c62aee
Nikhil Buduma <nkbuduma@gmail.com>
2016-12-23 16:21:34 -0800

    rm panic

--- rabbit.go
+++ rabbit.go
@@ -13,7 +13,7 @@ import (
 
 func failOnError(err error, msg string) {
 	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
+		log.Printf("%s: %s", msg, err)
 		// panic(fmt.Sprintf("%s: %s", msg, err))
 	}
 }
