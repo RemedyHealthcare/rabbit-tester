da288cf0dbe94e152a3789622d0b6277ee9f53e9
Nikhil Buduma <nkbuduma@gmail.com>
2016-12-23 16:00:36 -0800

    change heartbeat

--- rabbit.go
+++ rabbit.go
@@ -43,7 +43,7 @@ func main() {
 	if rabbitURL == "" {
 		rabbitURL = "amqp://guest:guest@localhost:5672/"
 	}
-	conn, err := amqp.Dial(rabbitURL)
+	conn, err := amqp.DialConfig(rabbitURL, amqp.Config{Heartbeat: time.Second * 60})
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
